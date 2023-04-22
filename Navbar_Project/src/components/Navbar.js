9620aeff14c821d34712838a9b05c1d0a58d960d
Gerard <dothanhvinh1@gmail.com>
2023-04-22 18:17:57 +0700

    Complete Navbar Project

---
+++ Navbar_Project/src/components/Navbar.js
@@ -0,0 +1,65 @@
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "../data";
+import logo from "../logo.svg";
+
+const Navbar = () => {
+  const [linkArray, setLinkArray] = useState(links);
+  const [icons, setIcons] = useState(social);
+  const [showLink, setShowLink] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(0);
+  const links_section = linkArray.map((link) => {
+    const { id, url, text } = link;
+    return (
+      <li key={id}>
+        <a href={url}>{text}</a>
+      </li>
+    );
+  });
+
+  const social_section = icons.map((social) => {
+    const { id, url, icon } = social;
+    return (
+      <li key={id}>
+        <a href={url}>{icon}</a>
+      </li>
+    );
+  });
+
+  function toggleLinks() {
+    setShowLink(!showLink);
+  }
+
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => {
+        setWindowWidth(window.innerWidth);
+        console.log(windowWidth);
+        if (windowWidth >= 900) setShowLink(false);
+      },
+      []
+    );
+  });
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="logo" alt="logo" />
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container">
+          <ul className="links">{links_section}</ul>
+        </div>
+        <div>
+          <ul className="social-icons">{social_section}</ul>
+        </div>
+      </div>
+      {showLink && <ul className="links">{links_section}</ul>}
+    </nav>
+  );
+};
+
+export default Navbar;
